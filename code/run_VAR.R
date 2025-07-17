194a3c39f5d0f3237bcf9e52952ef5b10e679b4a
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2025-07-17 16:33:43 -0700

    Rework VAR sims

--- code/run_VAR.R
+++ code/run_VAR.R
@@ -11,28 +11,23 @@ sem = "
   moose -> moose, 1, arM
 "
 
+# initial fit to build model with ln_sigma fixed
 map <- list(lnsigma_j=factor(c(NA,NA)))
-
-# pad NAs to build a long time-series of simulated data below
-na_mat <- matrix(NA, nrow=2048, ncol=2)
-#na_mat <- matrix(NA, nrow=20, ncol=2)
-data2 <- ts(rbind(data, na_mat))
 fit0 = dsem( sem = sem,
              family=family,
-             tsdata = data2,
+             tsdata = data,
              estimate_delta0 = FALSE,
              control = dsem_control(
                quiet = FALSE,
                run_model=FALSE,
                getsd = FALSE) )
-
 parameters = fit0$obj$env$parList()
 parameters$lnsigma_j <- rep(log(.1),2)
 
 # initial fit to get good starting values
 fit0 = dsem( sem = sem,
              family=family,
-             tsdata = data2,
+             tsdata = data,
              estimate_delta0 = FALSE,
              control = dsem_control(
                parameters=parameters,
@@ -41,37 +36,30 @@ fit0 = dsem( sem = sem,
                map=map,
                getsd = FALSE) )
 parameters = fit0$obj$env$parList()
-parameters$delta0_j = rep(0, ncol(data2) )
+parameters$delta0_j = rep(0, ncol(data) )
 
 # Refit with delta0
 fit = dsem( sem = sem,
             family=family,
-               tsdata = data2,
-               estimate_delta0 = TRUE,
-               control = dsem_control( quiet=TRUE,
-                                       map=map,
-                                       parameters = parameters ) )
+            tsdata = data,
+            estimate_delta0 = TRUE,
+            control = dsem_control( quiet=TRUE,
+                                    map=map,
+                                    parameters = parameters ) )
+parameters = fit$obj$env$parList()
 
-
-# this is the full simulated data set, which can be
-datsim <- simulate(fit, nsim=1, seed=123,
-                   variance='random',fill_missing = TRUE,
-                   resimulate_gmrf = TRUE)[[1]]
-matplot(datsim)
-
-#test <- sample_sparse_tmb(obj, iter=1000)
-# test
-# pairs_admb(test, order='slow', pars=1:5)
-#plot_Q(test)
-
-options(future.globals.maxSize= 2000*1024^2) # 1000 MB limit
-metrics <- c('unit', 'diag', 'dense', 'sparse')
+options(future.globals.maxSize= 5000*1024^2) # 1000 MB limit
+metrics <- c('unit', 'auto')
 stats <- list()
-for(nyrs in c(100,250,375, 500, 750, 1500)){
+for(nyrs in  c(100,250,375, 500, 750, 1500)){
   message("Starting analysis for nyrs=",nyrs)
-  dattmp <- ts(datsim[1:nyrs,])
+  # pad NAs to build a long time-series of simulated data below
+  na_mat <- matrix(NA, nrow=nyrs-61, ncol=2)
+  dattmp <- ts(rbind(data, na_mat))
   partmp <- parameters
-  partmp$x_tj <- partmp$x_tj[1:nyrs,]
+  partmp$x_tj <- matrix(0, ncol=2, nrow=nrow(dattmp))
+  message("Simulating data..")
+  # get dimensions right
   fitsim = dsem( sem = sem,
                  family=family,
                  tsdata = dattmp,
@@ -79,11 +67,22 @@ for(nyrs in c(100,250,375, 500, 750, 1500)){
                  control = dsem_control( quiet=TRUE,
                                          map=map,
                                          parameters = partmp ) )
+  # simulate a whole new data set
+  datsim <- simulate(fitsim, nsim=1, seed=nyrs,
+                     variance='random',fill_missing = TRUE,
+                     resimulate_gmrf = TRUE)[[1]]
+  # rebuild to get the data into the obj for MCMC sampling
+  fitsim <- dsem(sem = sem, family=family, tsdata = datsim,
+                 estimate_delta0 = TRUE,
+                 control = dsem_control(quiet=TRUE,
+                                         map=map,
+                                         parameters = partmp ) )
   obj <- fitsim$obj
   fits <- fit_models(obj, chains=4, cores=4,
                      metrics=metrics,
                      iter=2000,
                      init='last.par.best',
+                     control=list(adapt_delta=.95,max_treedepth=1),
                      replicates=reps, cpus=cpus,
                      model='VAR', plot=FALSE)
   nrepars <- 2*nyrs
@@ -91,7 +90,7 @@ for(nyrs in c(100,250,375, 500, 750, 1500)){
   saveRDS(stats, file='results/VAR_stats.RDS')
   g <- ggplot(stats, aes(nrepars, eff, group=interaction(metric,replicate), color=metric)) +
     geom_point(alpha=.5) +
-    geom_line() +
+    geom_line() + scale_x_log10()+
     scale_y_log10() + labs(x='# of random effects', y='Efficiency (ESS/t)')
   ggsave('plots/VAR_stats.png', g, width=7, height=5)
   print(g)
