fc5efd50fce06cea144cb5b82bb0166e886978f4
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2025-07-21 07:16:29 -0700

    Rerun glmmTMB simulation

--- code/run_glmmTMB.R
+++ code/run_glmmTMB.R
@@ -11,6 +11,8 @@ mod = glmmTMB(count~spp * mined + (1|site), Salamanders, family="nbinom2")
 
 
 # code from Mollie Brooks from the glmmTMB paper
+
+# this makes a list with 512 replicates of the original data set with 644 rows
 sims1 = lapply(simulate(mod, nsim=512, seed=111),
                function(count){
                  cbind(count, Salamanders[,c('site', 'mined', 'spp')])
@@ -18,27 +20,33 @@ sims1 = lapply(simulate(mod, nsim=512, seed=111),
 
 n = nrow(Salamanders)
 nRE=length(unique(Salamanders$site))
+sitereps = c(2, 4,8, 16, 32,64, 128, 256)
 bigdat0 = lapply(sitereps, function(x) do.call(rbind, sims1[1:x]))
+# make the sites a new unique factor
 bigdat =  lapply(1:length(sitereps), function(x)
   data.frame(bigdat0[[x]], "grp"=paste0(bigdat0[[x]]$site, rep(1:sitereps[x], each=n))))
+sapply(bigdat, \(x) length(unique(x$grp)))
 
 options(future.globals.maxSize= 4000*1024^2) # 4000 MB limit
-metrics <- c('unit', 'diag', 'dense', 'sparse')
+metrics <- c('unit', 'auto')
 stats <- list()
-sitereps = c(2, 4,8, 16, 32,64, 128, 256)
-for(ii in 5:length(sitereps)){
+for(ii in 1:length(sitereps)){
   nreps <- sitereps[ii]
   set.seed(nreps)
   message("Starting analysis for nreps=",nreps)
   dat2 <- bigdat[[ii]]
-  obj <- glmmTMB(count ~ spp * mined + (1 | grp), data = dat2, family="nbinom2")$obj
+  # turn off estimation inside glmmTMB since redoing it below, ignore converg warnings
+  suppressWarnings(
+    obj <- glmmTMB(count ~ spp * mined + (1 | grp), data = dat2, family="nbinom2", se=FALSE,
+                 control = glmmTMBControl(optCtrl=list(iter.max=1,eval.max=1)))$obj
+    )
+  nrepars <- length(obj$env$random)
   fits <- fit_models(obj, chains=4, cores=4,
                      metrics=metrics,
                      iter=2000,
                      init='random', replicates=reps,
                      cpus=ifelse(nreps<=128, cpus,1),
                      model='glmmTMB', plot=FALSE)
-  nrepars <- length(obj$env$random)
   stats <- rbind(stats, cbind(nreps=nreps, nrepars=nrepars, get_stats(fits)))
   saveRDS(stats, file='results/glmmTMB_stats.RDS')
   g <- ggplot(stats, aes(nrepars, eff, group=interaction(metric,replicate), color=metric)) +
