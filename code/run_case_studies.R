2f1a0fb0cb887e876ebc040794c6de7386af24b7
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2025-08-26 09:55:41 -0700

    Prepare to run case studies again

--- code/run_case_studies.R
+++ code/run_case_studies.R
@@ -1,91 +1,115 @@
+
+dir.create('results/case_studies', showWarnings=FALSE)
+dir.create('plots/case_studies', showWarnings=FALSE)
+
+
 source('code/load_TMB_objects.R')
 reps <- 1:3 # vector of replicate analyses
 cpus <- 1 # parallel sessions for parallel chains!
 
 
-# fits.simple <- fit_models(obj.simple, iter=2000,
-#                           cpus=cpus, replicates=reps)
-fits.sdmTMB <- fit_models(obj.sdmTMB, iter=2000,
+# save files to separate folder
+out <- 'results/case_studies'
+
+fits.sdmTMB <- fit_models(obj.sdmTMB,
                           control=list(adapt_delta=.9),
-                          cpus=cpus, replicates=reps)
-fits.pollock <- fit_models(obj.pollock, iter=2000,
-                           init='last.par.best',
-                           cpus=cpus, replicates=reps)
-fits.wildf <- fit_models(obj.wildf, iter=2000, cpus=cpus,
-                         init='last.par.best',
-                         replicates=reps, control=list(adapt_delta=.95))
-# turn on metric adaptation
-fits.wildf_adapted <- fit_models(obj.wildf, iter=2000, cpus=cpus,
+                          cpus=cpus, replicates=reps,
+                          outpath=out)
+fits.pollock <- fit_models(obj.pollock,
+                         #  init='last.par.best',
+                           cpus=cpus, replicates=reps,
+                           outpath=out)
+fits.wildf <- fit_models(obj.wildf,  cpus=cpus,
+                        # init='last.par.best',
                          replicates=reps, control=list(adapt_delta=.95),
-                         init='last.par.best',
-                         adapt_metric = TRUE, warmup=1000, model='wildf_adapted')
+                         outpath=out)
+# turn on metric adaptation
+fits.wildf_adapted <- fit_models(obj.wildf,  cpus=cpus,
+                         replicates=reps, control=list(adapt_delta=.95),
+                       #  init='last.par.best',
+                         adapt_metric = TRUE, warmup=1000, model='wildf_adapted',
+                         outpath=out)
 # this one has some inits fail so fall back to mode
-fits.swallows <- fit_models(obj.swallows, iter=2000, cpus=cpus,
+fits.swallows <- fit_models(obj.swallows,  cpus=cpus,
                             replicates=reps, init='last.par.best',
-                            control=list(adapt_delta=.99))
-fits.dlm <- fit_models(obj.dlm, iter=2000, cpus=cpus,
-                       replicates=reps, model='dlm')
-fits.salamanders <- fit_models(obj.salamanders, iter=2000, cpus=cpus,
-                       replicates=reps, model='salamanders')
-fits.gp_pois_regr <- fit_models(obj.gp_pois_regr, iter=2000, cpus=cpus,
+                            control=list(adapt_delta=.99),
+                            outpath=out)
+fits.dlm <- fit_models(obj.dlm,  cpus=cpus,
+                       replicates=reps, model='dlm',
+                       outpath=out)
+fits.salamanders <- fit_models(obj.salamanders,  cpus=cpus,
+                       replicates=reps, model='salamanders',
+                       outpath=out)
+fits.gp_pois_regr <- fit_models(obj.gp_pois_regr,  cpus=cpus,
                                replicates=reps, model='gp_pois_regr',
-                               control=list(adapt_delta=.99))
-fits.petrel <- fit_models(obj.petrel, iter=2000, cpus=cpus,
-                          init='last.par.best',
-                          replicates=reps, model='petrel')
-fits.wham <- fit_models(obj.wham, iter=2000, cpus=cpus,
-                        init='last.par.best',
+                               control=list(adapt_delta=.99),
+                               outpath=out)
+fits.petrel <- fit_models(obj.petrel,  cpus=cpus,
+                        #  init='last.par.best',
+                          replicates=reps, model='petrel',
+                          outpath=out)
+fits.wham <- fit_models(obj.wham,  cpus=cpus,
+                      #  init='last.par.best',
                         control=list(adapt_delta=.9),
-                        replicates=reps, model='wham')
-fits.sam <- fit_models(obj.sam, iter=2000, cpus=cpus,
-                        init='last.par.best',
+                        replicates=reps, model='wham',
+                        outpath=out)
+fits.sam <- fit_models(obj.sam,  cpus=cpus,
+                     #   init='last.par.best',
                         control=list(adapt_delta=.9),
-                        replicates=reps, model='sam')
+                        replicates=reps, model='sam',
+                       outpath=out)
 
 # Rerun with laplace turned on, called "embedded laplace
 # approximation" (ELA) by Margossian et al
-fits.sdmTMB_ELA <- fit_models(obj.sdmTMB, iter=2000,
+fits.sdmTMB_ELA <- fit_models(obj.sdmTMB,
                           control=list(adapt_delta=.9),
                           cpus=cpus, replicates=reps,
                           laplace=TRUE,
-                          model='sdmTMB_ELA')
-# fits.pollock_ELA <- fit_models(obj.pollock, iter=2000,
+                          model='sdmTMB_ELA',
+                          outpath=out)
+# fits.pollock_ELA <- fit_models(obj.pollock,
 #                            cpus=cpus, replicates=reps,
 #                            model='pollock_ELA', laplace=TRUE)
-fits.wildf_ELA <- fit_models(obj.wildf, iter=2000, cpus=cpus,
-                         init='last.par.best',
+fits.wildf_ELA <- fit_models(obj.wildf,  cpus=cpus,
+                         #init='last.par.best',
                          replicates=reps, control=list(adapt_delta=.95),
-                         model='wildf_ELA', laplace=TRUE)
+                         model='wildf_ELA', laplace=TRUE,
+                         outpath=out)
 # this one has some inits fail so fall back to mode
-fits.swallows_ELA <- fit_models(obj.swallows, iter=2000, cpus=cpus,
+fits.swallows_ELA <- fit_models(obj.swallows,  cpus=cpus,
                             replicates=reps, init='last.par.best',
                             control=list(adapt_delta=.9),
-                            laplace=TRUE, model='swallows_ELA')
-fits.dlm_ELA <- fit_models(obj.dlm, iter=2000, cpus=cpus,
+                            laplace=TRUE, model='swallows_ELA',
+                            outpath=out)
+fits.dlm_ELA <- fit_models(obj.dlm,  cpus=cpus,
                        laplace=TRUE,
-                       replicates=reps, model='dlm_ELA')
-fits.salamanders_ELA <- fit_models(obj.salamanders, iter=2000, cpus=cpus,
+                       replicates=reps, model='dlm_ELA',
+                       outpath=out)
+fits.salamanders_ELA <- fit_models(obj.salamanders,  cpus=cpus,
                                replicates=reps, model='salamanders_ELA',
-                               laplace=TRUE)
-fits.gp_pois_regr_ELA <- fit_models(obj.gp_pois_regr, iter=2000, cpus=cpus,
+                               laplace=TRUE,
+                               outpath=out)
+fits.gp_pois_regr_ELA <- fit_models(obj.gp_pois_regr,  cpus=cpus,
                                 warmup=1000,
                                 replicates=reps, model='gp_pois_regr_ELA',
                                 control=list(adapt_delta=.99),
-                                laplace=TRUE)
+                                laplace=TRUE, outpath=out)
 # why is this sooo slow??
 # 1000 transitions using 10 leapfrog steps per transition would take 10853.6 seconds.
-# fits.petrel_ELA <- fit_models(obj.petrel, iter=2000, cpus=cpus,
+# fits.petrel_ELA <- fit_models(obj.petrel,  cpus=cpus,
 #                                laplace=TRUE, init='last.par.best',
-#                            replicates=reps, model='petrel_ELA')
+#                            replicates=reps, model='petrel_ELA', outpath=out)
 ## crazy slow again
-# fits.wham_ELA <- fit_models(obj.wham, iter=2000, cpus=cpus,
+# fits.wham_ELA <- fit_models(obj.wham,  cpus=cpus,
 #                         init='last.par.best', laplace=TRUE,
 #                         control=list(adapt_delta=.9),
-#                         replicates=reps, model='wham_ELA')
-fits.sam_ELA <- fit_models(obj.sam, iter=2000, cpus=cpus,
-                       init='last.par.best', laplace=TRUE,
+#                         replicates=reps, model='wham_ELA', outpath=out)
+fits.sam_ELA <- fit_models(obj.sam,  cpus=cpus,
+                       #init='last.par.best',
+                       laplace=TRUE,
                        control=list(adapt_delta=.9),
-                       replicates=reps, model='sam_ELA')
+                       replicates=reps, model='sam_ELA',
+                       outpath=out)
 
 
 
@@ -94,72 +118,88 @@ fits.sam_ELA <- fit_models(obj.sam, iter=2000, cpus=cpus,
 source('code/load_RTMB_objects.R')
 reps <- 1:3 # vector of replicate analyses
 cpus <- 1 # parallel sessions for parallel chains!
-fits.schools <- fit_models(obj.schools, iter=2000,
+fits.schools <- fit_models(obj.schools,
                            cpus=cpus, replicates=reps,
                            globals=list(schools_dat=schools_dat),
-                           model='schools')
+                           model='schools',
+                           outpath=out)
 fits.diamonds <- fit_models(obj.diamonds, replicates=reps,
-                            cpus=cpus, iter=2000,
+                            cpus=cpus,
                             globals=list(diamonds_dat=diamonds_dat),
-                            model='diamonds')
+                            model='diamonds',
+                            outpath=out)
 fits.radon <- fit_models(obj.radon, replicates=reps, cpus=cpus,
-                         iter=2000, globals=list(radon_dat=radon_dat),
+                          globals=list(radon_dat=radon_dat),
                          control=list(adapt_delta=.9),
-                         model='radon')
+                         model='radon',
+                         outpath=out)
 fits.kilpisjarvi <- fit_models(obj.kilpisjarvi, replicates=reps,
-                               cpus=cpus, iter=2000,
+                               cpus=cpus,
                                globals=list(kilpisjarvi_dat=kilpisjarvi_dat),
                                control=list(adapt_delta=.9),
-                               model='kilpisjarvi')
-fits.causal <- fit_models(obj.causal, iter=2000, cpus=cpus, replicates=reps,
-                          model='causal', init='last.par.best',
-                          control=list(adapt_delta=.9))
-fits.irt_2pl <- fit_models(obj.irt_2pl, iter=2000, cpus=cpus, replicates=reps,
-                          model='irt_2pl', init='last.par.best',
+                               model='kilpisjarvi',
+                               outpath=out)
+fits.causal <- fit_models(obj.causal,  cpus=cpus, replicates=reps,
+                          model='causal',
+                          #init='last.par.best',
+                          control=list(adapt_delta=.9),
+                          outpath=out)
+fits.irt_2pl <- fit_models(obj.irt_2pl,  cpus=cpus, replicates=reps,
+                          model='irt_2pl',
+                          #init='last.par.best',
                           globals=list(irt_2pl_dat=irt_2pl_dat),
-                          control=list(adapt_delta=.9))
-fits.irt_2pl_nc <- fit_models(obj.irt_2pl_nc, iter=2000, cpus=cpus, replicates=reps,
-                           model='irt_2pl_nc', init='last.par.best',
+                          control=list(adapt_delta=.9),
+                          outpath=out)
+fits.irt_2pl_nc <- fit_models(obj.irt_2pl_nc,  cpus=cpus, replicates=reps,
+                           model='irt_2pl_nc',
+                           #init='last.par.best',
                            globals=list(irt_2pl_dat=irt_2pl_dat),
-                           control=list(adapt_delta=.9))
+                           control=list(adapt_delta=.9),
+                           outpath=out)
 fits.lynx <- fit_models(obj.lynx, replicates=reps, cpus=cpus,
-                         iter=2000, globals=globals.lynx,
-                         control=list(adapt_delta=.9), init='last.par.best',
-                         model='lynx')
+                        globals=globals.lynx,
+                        control=list(adapt_delta=.9),
+                        #init='last.par.best',
+                        model='lynx',
+                        outpath=out)
 
 # Rerun with laplace turned on, called "embedded laplace
 # approximation" (ELA) by Margossian et al
-fits.schools_ELA <- fit_models(obj.schools, iter=2000,
+fits.schools_ELA <- fit_models(obj.schools,
                            cpus=cpus, replicates=reps,
                            laplace=TRUE,
                            globals=list(schools_dat=schools_dat),
-                           model='schools_ELA')
+                           model='schools_ELA', outpath=out)
 fits.diamonds_ELA <- fit_models(obj.diamonds, replicates=reps,
-                            cpus=cpus, iter=2000,
-                            laplace=TRUE, init='last.par.best',
+                            cpus=cpus,
+                            laplace=TRUE, #init='last.par.best',
                             globals=list(diamonds_dat=diamonds_dat),
-                            model='diamonds_ELA')
+                            model='diamonds_ELA', outpath=out)
 fits.radon_ELA <- fit_models(obj.radon, replicates=reps, cpus=cpus,
-                         iter=2000, globals=list(radon_dat=radon_dat),
+                          globals=list(radon_dat=radon_dat),
                          control=list(adapt_delta=.9),
                          laplace=TRUE,
-                         model='radon_ELA')
-fits.causal_ELA <- fit_models(obj.causal, iter=2000, cpus=cpus, replicates=reps,
-                          model='causal_ELA', init='last.par.best',
+                         model='radon_ELA',
+                         outpath=out)
+fits.causal_ELA <- fit_models(obj.causal,  cpus=cpus, replicates=reps,
+                          model='causal_ELA', #init='last.par.best',
                           laplace=TRUE,
-                          control=list(adapt_delta=.9))
-fits.irt_2pl_ELA <- fit_models(obj.irt_2pl, iter=2000, cpus=cpus, replicates=reps,
-                           model='irt_2pl_ELA', init='random',
+                          control=list(adapt_delta=.9),
+                          outpath=out)
+fits.irt_2pl_ELA <- fit_models(obj.irt_2pl,  cpus=cpus, replicates=reps,
+                           model='irt_2pl_ELA',
                            laplace=TRUE,
                            globals=list(irt_2pl_dat=irt_2pl_dat),
-                           control=list(adapt_delta=.9))
-fits.irt_2pl_nc_ELA <- fit_models(obj.irt_2pl_nc, iter=2000, cpus=cpus, replicates=reps,
-                               model='irt_2pl_nc_ELA', init='random',
+                           control=list(adapt_delta=.9),
+                           outpath=out)
+fits.irt_2pl_nc_ELA <- fit_models(obj.irt_2pl_nc,  cpus=cpus, replicates=reps,
+                               model='irt_2pl_nc_ELA',
                                laplace=TRUE,
                                globals=list(irt_2pl_dat=irt_2pl_dat),
-                               control=list(adapt_delta=.9))
+                               control=list(adapt_delta=.9),
+                               outpath=out)
 fits.lynx_ELA <- fit_models(obj.lynx, replicates=reps, cpus=cpus,
                             laplace=TRUE,
-                            iter=2000, globals=globals.lynx,
-                            control=list(adapt_delta=.9), init='last.par.best',
-                            model='lynx_ELA')
+                             globals=globals.lynx,
+                            control=list(adapt_delta=.9), #init='last.par.best',
+                            model='lynx_ELA', outpath=out)
